46773b6fe953e6348ad787b131b094d5156c6a97
vierifath <vieri.ayuba@gmail.com>
2020-10-29 01:02:59 +0700

    add bookingform (vieri)

--- laravel/resources/views/room/BookingForm.blade.php
+++ laravel/resources/views/room/BookingForm.blade.php
@@ -3,51 +3,97 @@
 @section('container')
 
 <div class="container">
+<div class="FormReservation"><h1>Form Reservation</h1></div>
 <form>
     <div class="form-row">
       <div class="form-group col-md-6">
-        <label for="inputEmail4">Email</label>
-        <input type="email" class="form-control" id="inputEmail4">
+        <label for="inputID">ID Customer</label>
+        <input type="number" class="form-control" id="inputID">
       </div>
+      
       <div class="form-group col-md-6">
-        <label for="inputPassword4">Password</label>
-        <input type="password" class="form-control" id="inputPassword4">
+        <label for="inputName">Name</label>
+        <input type="text" class="form-control" id="InputName">
       </div>
     </div>
+    
+    
+    <div class="form-group">
+      <label for="inputEmail">E-mail</label>
+      <input type="text" class="form-control" id="inputEmail">
+    </div>
+
     <div class="form-group">
       <label for="inputAddress">Address</label>
-      <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+      <input type="text" class="form-control" id="inputAddress">
     </div>
-    <div class="form-group">
-      <label for="inputAddress2">Address 2</label>
-      <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
-    </div>
-    <div class="form-row">
-      <div class="form-group col-md-6">
-        <label for="inputCity">City</label>
-        <input type="text" class="form-control" id="inputCity">
+
+
+
+      <div class="form-group">
+        <label for="inputPhoneNumber">Phone Number</label>
+        <input type="number" class="form-control" id="inputPN">
       </div>
-      <div class="form-group col-md-4">
-        <label for="inputState">State</label>
-        <select id="inputState" class="form-control">
-          <option selected>Choose...</option>
-          <option>...</option>
-        </select>
-      </div>
-      <div class="form-group col-md-2">
-        <label for="inputZip">Zip</label>
-        <input type="text" class="form-control" id="inputZip">
+      
+
+    <div class="form-group row">
+      <label for="example-datetime-local-input" class="col-2 col-form-label">CheckIn (Date/Time)</label>
+      <div class="col-10">
+        <input class="form-control" type="datetime-local" value="2020-10-19T13:45:00" id="example-datetime-local-input">
       </div>
     </div>
+
+    <div class="form-group row">
+      <label for="example-datetime-local-input" class="col-2 col-form-label">CheckOut (Date/Time)</label>
+      <div class="col-10">
+        <input class="form-control" type="datetime-local" value="2020-10-20T13:45:00" id="example-datetime-local-input">
+      </div>
+    </div>
+
+    <div class="form-group row">
+      <label for="example-datetime-local-input" class="col-2 col-form-label">Payment Method</label>
+    <div class="col-10">
+      <select class="custom-select">
+        <option value="1">DebitCard</option>
+        <option value="2">CreditCard</option>
+      </select>
+    </div>
+     </div>
+    
     <div class="form-group">
       <div class="form-check">
         <input class="form-check-input" type="checkbox" id="gridCheck">
         <label class="form-check-label" for="gridCheck">
-          Check me out
+          I accept the Term of Use & Privacy Policy
         </label>
       </div>
     </div>
-    <button type="submit" class="btn btn-primary">Sign in</button>
+    <button type="button" class="btn btn-success" data-toggle= "modal" data-target="#exampleModal">Reserve Now !</button>
+    
   </form>
+
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Validation Data</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      Are you sure your data is valid ?
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">No</button>
+        <button type="button" class="btn btn-primary">Yes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
 </div>
 @endsection
--- laravel/resources/views/room/index.blade.php
+++ laravel/resources/views/room/index.blade.php
@@ -10,22 +10,208 @@
             <table class="table">
                 <thead class="thead-dark">
                     <tr>
-                        <th scope="col">NO</th>
+                        <th scope="col">Room Number</th>
                         <th scope="col">Room Type</th>
+                        <th scope="col">Floor Number</th>
                         <th scope="col">Price</th>
-                        <th scope="col"></th>
+                        <th scope="col">Action</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Kings Room</td>
+                    
+                <tr class="PS-001">
+                        <th scope="row1">001</th>
+                        <td>Presidential Suite Room</td>
+                        <td>1</td>
+                        <td>Rp 5.000.000/night</td>
+                        <td> 
+                            <a href="" class="btn btn-primary">View</a>   
+                            <a href="/form" class="btn btn-success">Book</a>   
+                        </td>
+                    </tr>
+
+                    
+                    <tr class="PS-002">
+                        <th scope="row1">002</th>
+                        <td>Presidential Suite Room</td>
+                        <td>1</td>
+                        <td>Rp 5.000.000/night</td>
+                        <td> 
+                            <a href="" class="btn btn-primary">View</a>   
+                            <a href="/form" class="btn btn-success">Book</a>   
+                        </td>
+                    </tr>
+
+
+                    <tr class="King-003">
+                        <th scope="row1">003</th>
+                        <td>Deluxe Kings Room</td>
+                        <td>2</td>
                         <td>Rp 2.000.000/night</td>
                         <td> 
                             <a href="" class="btn btn-primary">View</a>   
                             <a href="/form" class="btn btn-success">Book</a>   
                         </td>
                     </tr>
+
+                    <tr class="King-004">
+                        <th scope="row1">004</th>
+                        <td>Deluxe Kings Room</td>
+                        <td>2</td>
+                        <td>Rp 2.000.000/night</td>
+                        <td> 
+                            <a href="" class="btn btn-primary">View</a>   
+                            <a href="/form" class="btn btn-success">Book</a>   
+                        </td>
+                    </tr>
+
+                    
+                    <tr class="Queen-005">
+                        <th scope="row1">005</th>
+                        <td>Superior Queen Room</td>
+                        <td>3</td>
+                        <td>Rp 1.500.000/night</td>
+                        <td> 
+                            <a href="" class="btn btn-primary">View</a>   
+                            <a href="/form" class="btn btn-success">Book</a>   
+                        </td>
+                    </tr>
+                    
+                    <tr class="Queen-006">
+                        <th scope="row1">006</th>
+                        <td>Superior Queen Room</td>
+                        <td>3</td>
+                        <td>Rp 1.500.000/night</td>
+                        <td> 
+                            <a href="" class="btn btn-primary">View</a>   
+                            <a href="/form" class="btn btn-success">Book</a>   
+                        </td>
+                    </tr>
+
+                    <tr class="Queen-007">
+                        <th scope="row1">007</th>
+                        <td>Superior Queen Room</td>
+                        <td>3</td>
+                        <td>Rp 1.500.000/night</td>
+                        <td> 
+                            <a href="" class="btn btn-primary">View</a>   
+                            <a href="/form" class="btn btn-success">Book</a>   
+                        </td>
+                    </tr>
+                    
+
+
+                    <tr class="O-008">
+                        <th scope="row1">008</th>
+                        <td>Standart Double Room</td>
+                        <td>4</td>
+                        <td>Rp 750.000/night</td>
+                        <td> 
+                            <a href="" class="btn btn-primary">View</a>   
+                            <a href="/form" class="btn btn-success">Book</a>   
+                        </td>
+                    </tr>
+
+                    
+                    <tr class="O-009">
+                        <th scope="row1">009</th>
+                        <td>Standart Double Room</td>
+                        <td>4</td>
+                        <td>Rp 750.000/night</td>
+                        <td> 
+                            <a href="" class="btn btn-primary">View</a>   
+                            <a href="/form" class="btn btn-success">Book</a>   
+                        </td>
+                    </tr>
+
+                    
+                    <tr class="O-009">
+                        <th scope="row1">010</th>
+                        <td>Standart Double Room</td>
+                        <td>4</td>
+                        <td>Rp 750.000/night</td>
+                        <td> 
+                            <a href="" class="btn btn-primary">View</a>   
+                            <a href="/form" class="btn btn-success">Book</a>   
+                        </td>
+                    </tr>
+
+                    
+                    <tr class="O-009">
+                        <th scope="row1">011</th>
+                        <td>Standart Double Room</td>
+                        <td>4</td>
+                        <td>Rp 750.000/night</td>
+                        <td> 
+                            <a href="" class="btn btn-primary">View</a>   
+                            <a href="/form" class="btn btn-success">Book</a>   
+                        </td>
+                    </tr>
+
+                    
+                    <tr class="O-009">
+                        <th scope="row1">012</th>
+                        <td>Standart Double Room</td>
+                        <td>4</td>
+                        <td>Rp 750.000/night</td>
+                        <td> 
+                            <a href="" class="btn btn-primary">View</a>   
+                            <a href="/form" class="btn btn-success">Book</a>   
+                        </td>
+                    </tr>
+
+                    <tr class="O-009">
+                        <th scope="row1">013</th>
+                        <td>Standart Single Room</td>
+                        <td>5</td>
+                        <td>Rp 500.000/night</td>
+                        <td> 
+                            <a href="" class="btn btn-primary">View</a>   
+                            <a href="/form" class="btn btn-success">Book</a>   
+                        </td>
+                    </tr>
+
+                    
+                    <tr class="O-009">
+                        <th scope="row1">014</th>
+                        <td>Standart Single Room</td>
+                        <td>5</td>
+                        <td>Rp 500.000/night</td>
+                        <td> 
+                            <a href="" class="btn btn-primary">View</a>   
+                            <a href="/form" class="btn btn-success">Book</a>   
+                        </td>
+                    </tr>
+
+                    
+                    <tr class="O-009">
+                        <th scope="row1">015</th>
+                        <td>Standart Single Room</td>
+                        <td>5</td>
+                        <td>Rp 500.000/night</td>
+                        <td> 
+                            <a href="" class="btn btn-primary">View</a>   
+                            <a href="/form" class="btn btn-success">Book</a>   
+                        </td>
+                    </tr>
+
+
+
+
+                   
+
+
+
+
+
+
+
+
+
+
+
+
+
                 </tbody>
             </table>
        </div>
