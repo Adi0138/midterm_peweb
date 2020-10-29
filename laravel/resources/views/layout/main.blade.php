6cc5ad3fe6d7500b7f97c935b9549c37d76b4320
Adi0138 <gemaadi9@gmail.com>
2020-10-29 21:13:56 +0700

    update anyar

--- laravel/resources/views/layout/main.blade.php
+++ laravel/resources/views/layout/main.blade.php
@@ -11,7 +11,7 @@
     <title>@yield('title')</title>
   </head>
   <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-info">
         <div class="container">
             <a class="navbar-brand" href="#">ITS Hotel</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -26,6 +26,19 @@
                 </div>
             </div>
         </div>
+        <div class="flex-center position-ref full-height">
+          @if (Route::has('login'))
+              <div class="top-right links">
+                  @auth
+                      <a href="{{ url('/home') }}">Home</a>
+                  @else
+                      <a href="{{ route('login') }}">Login</a>
+                      @if (Route::has('register'))
+                          <a href="{{ route('register') }}">Register</a>
+                      @endif
+                  @endauth
+              </div>
+          @endif
     </nav>
 
     @yield('container')
